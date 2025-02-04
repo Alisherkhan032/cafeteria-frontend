7b0ee62e024a6e101bf33ade33f3310bb4543480
alisherkhann <sherkhanali032@gmail.com>
2025-02-04 17:02:30 +0530

    remove state after logout

--- src/slices/counterSlice.js
+++ src/slices/counterSlice.js
@@ -50,6 +50,12 @@ const counterSlice = createSlice({
     removeCounter: (state, action) => {
       state.counters = state.counters.filter(counter => counter._id !== action.payload);
     },
+    resetCounterSlice: (state, action) => {
+      state.counters = [];
+      state.dishes = [];
+      state.currentCounter = null;
+      state.loading = true;
+    }
   },
 });
 
@@ -58,6 +64,6 @@ export const selectCurrentCounter = (state)=> state.counter.currentCounter;
 export const dishesInCounter = (state)=> state.counter.dishes;
 export const selectloadingState = (state)=> state.counter.loading;
 
-export const {addCounter, setCounter,updateCounter, setLoading, setDishes, setCurrentCounter, updateDish, removeDish, addDish, removeCounter } = counterSlice.actions;
+export const {addCounter, setCounter,updateCounter, setLoading, setDishes, setCurrentCounter, updateDish, removeDish, addDish, removeCounter, resetCounterSlice } = counterSlice.actions;
 
 export default counterSlice.reducer;
